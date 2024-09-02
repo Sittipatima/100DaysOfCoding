1598e1eeb56424792e12081bdd2c1228ebf4e736
Sittipatima <sittipatima958@gmail.com>
2024-09-02 11:15:41 +0800

    Create Day1.java
    Biodata

---
+++ Day1.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+	System.out.println("Nama:Sitti Patima");
+	System.out.println("Alamat:Katumbangan");
+	System.out.println("Asal Sekolah:SMKN Labuang");
+	}
+}
