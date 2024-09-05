7a440624b4a289c8d44e297ab1a9b100e1837c0b
Sittipatima <sittipatima958@gmail.com>
2024-09-05 16:35:01 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,12 @@
+public class Day4 {
+	public static void main(String[] args) {
+	System.out.print ("Halo,perkenalkan " );
+	System.out.print ("nama saya  ");
+	System.out.print ("Fatima ");
+	System.out.printf (" saya dari %s","katumbangan, ");
+     System.out.printf ("umur saya %d tahun ", 18 );
+     System.out.printf (" dari kelas informatika %c",'B');
+     
+		}
+
+}
