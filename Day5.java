bcd5b06bc7593271ed6321a52c2f9e87a086bb91
Sittipatima <sittipatima958@gmail.com>
2024-09-06 13:41:24 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,7 @@
+public class dayy5 {
+	public static void main(String[] args) {
+//  contoh tipe data  adalah String dan int //
+	/*print digunakan untuk memunculkan variabel yang 
+    ada ditipe data yang digunakan*/
+	}
+}
