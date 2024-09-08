7397826b98800d8df2d79bc4eba08e762df5754f
Sittipatima <sittipatima958@gmail.com>
2024-09-08 21:01:30 +0800

    Create Dayy7.java

---
+++ Dayy7.java
@@ -0,0 +1,13 @@
+public class dayy7 {
+	public static void main(String[] args) {
+	double beratbadan = 54.8;
+	float tinggibadan = 150.5f;
+	char jeniskelamin = 'P';
+	boolean javaitususah = true;
+	
+	System.out.println ("Berat Badan: " + beratbadan );
+	System.out.println ("Tinggi Badan: " + tinggibadan );
+	System.out.println ("Jenis Kelamin: " + jeniskelamin );
+	System.out.println ("Java Itu Susah: " + javaitususah );
+	}
+    }
