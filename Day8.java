82678e3dfacb70583bc6e73b227f62499b8d986c
Sittipatima <sittipatima958@gmail.com>
2024-09-09 19:58:30 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,31 @@
+public class dayy8{
+	public static void main(String[] args) {
+	
+	String nama = "fati";
+	System.out.println ("sebelum diupdate "+ nama );
+	nama = "fatima";
+	System.out.println ("sesudah diupdate "+ nama );
+	
+	byte umur = 15;
+	System.out.println ("sebelum diupdate " + umur );
+	umur  = 18;
+	System.out.println ("sesudah diupdate " + umur);
+	
+	double beratbadan = 40.8;
+	System.out.println ("sebelum diupdate " + beratbadan );
+	beratbadan = 50.7;
+	System.out.println ("sesudah diupdate " + beratbadan );
+	
+	float tinggibadan = 140.9f;
+	System.out.println ("sebelum diupdate " + tinggibadan );
+	tinggibadan = 150.8f;
+	System.out.println ("sesudah diupdate " + tinggibadan );
+	
+	int nilaiutbk = 80;
+	System.out.println ("sebelum diupdate " + nilaiutbk );
+	nilaiutbk = 95;
+	System.out.println ("sesudah diupdate " + nilaiutbk );
+
+	
+	}
+}
