3f2c2a7aaa5d2a59ec82b03054c951f3907bf312
Sittipatima <sittipatima958@gmail.com>
2024-09-10 15:07:44 +0800

    Create Dayy9.java

---
+++ Dayy9.java
@@ -0,0 +1,15 @@
+public class dayy9{
+	public static void main(String[] args) {
+	final double pi = 3.14;
+	final float tb = 170.8f;
+	final byte umur = 20;
+	final int nilaiujian = 90;
+	final boolean javaitususah = true;
+	
+	System.out.println ("pi: " + pi );
+	System.out.println ("tb: " + tb );
+	System.out.println ("umur: " + umur );
+	System.out.println ("nilai ujian: " + nilaiujian);
+    System.out.println ("java itu susah: " + javaitususah );
+		}
+                      }
