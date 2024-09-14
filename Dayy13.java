58b32202acc344075ffb2cf2a8b0b4b6a889e0b1
Sittipatima <sittipatima958@gmail.com>
2024-09-14 10:57:40 +0800

    Dayy13.java

--- Dayy13.java
+++ Dayy13.java
@@ -1,7 +1,27 @@
-public class dayy13{
+public class dayy13 {
 	public static void main(String[] args) {
-	System.out.println ("DATA MAHASISWA 1");    
-    System.out.println ("Nama : Sitti Patima");
+	System.out.println ("Data Mahasiswa 1 ");	
+	System.out.println ("Nama\t\t: Sitti Patima ");
+     byte umur = 17;
+     int umurnenek = 79;
+     short umurkakek = 80;
+     System.out.println ("Umur\t\t: "+ umur );
+     System.out.println ("Umur Nenek\t: " + umurnenek );
+     System.out.println ("Umur Kakek\t: "+ umurkakek);
+     System.out.println ("~~~~~~~~~~~~~~~~");
+     System.out.println ("Data Mahasiswa 2");
+     System.out.println ("Nama\t\t: Sitti Patima");
+     umur = 18;
+     umurnenek = 90;
+     umurkakek = 95;
+     System.out.println("Umur\t\t: " + umur);
+     System.out.println ("Umur Nenek\t: "+ umurkakek );
+     System.out.println ("Umur Kakek\t: "+ umurkakek);
+     System.out.println ("~~~~~~~~~~~~~~~~~");
+     
+		}
+
+}
     
 	
 	}
