1489f3617a99c75b26eaf9f4ab0dcc1df71f5249
Sittipatima <sittipatima958@gmail.com>
2024-09-30 19:50:06 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+public class day29 {
+	public static void main(String[] args) {
+		
+		BigInteger banyakuangku = new BigInteger("500000000000");
+		System.out.println(" Banyak Uang Ku : " + banyakuangku);
+		
+	}
+}
