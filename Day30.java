e8d2fec57075023e04ed5576075cea2c6ad00b1b
Sittipatima <sittipatima958@gmail.com>
2024-10-01 22:33:23 +0800

    Update Day30.java

--- Day30.java
+++ Day30.java
@@ -2,7 +2,7 @@ import java.math.BigDecimal;
 public class day30 {
 public static void main(String[] args) {
 		
-   BigDecimal nilai = new BigDecimal ("134.9487288387");
+   BigDecimal nilai = new BigDecimal ("134.94872883876378276");
 	System.out.println ("Nilai : " + nilai);
 }
 }
