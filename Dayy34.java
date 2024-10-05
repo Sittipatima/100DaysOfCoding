107162ce72cf175f6c2d99b99af981a46c00b221
Sittipatima <sittipatima958@gmail.com>
2024-10-05 13:01:56 +0800

    Create Dayy34.java

---
+++ Dayy34.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class day34 {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+		System.out.print(" Masukkan Nama : ");
+		String nama = input.next();
+		System.out.print(" Masukkan Umur : ");
+		byte umur = input.nextByte();
+	}
+}
