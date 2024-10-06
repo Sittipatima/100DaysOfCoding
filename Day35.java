931e764ca486d10d17db36a873a4a8ef36e27352
Sittipatima <sittipatima958@gmail.com>
2024-10-06 20:56:17 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,10 @@
+public class day35 {
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 5;
+	    int c = a+b;
+	    int d = a-b;
+	    System.out.println(c);
+	    System.out.println(d);
+	}
+}
