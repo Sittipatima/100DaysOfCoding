db1e275b85d5b4be16497853d7a13a2e6f245053
Sittipatima <sittipatima958@gmail.com>
2024-10-07 21:28:25 +0800

    Create Day36.java

---
+++ Day36.java
@@ -0,0 +1,13 @@
+public class dayy36 {
+	public static void main(String[] args) {
+		int a = 25;
+		int b = 5;
+		int c = a*b;
+		int d = a/b;
+		int e = a%b;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+}
