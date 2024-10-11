c49c2f301d1bf9418a5deadf3877049c1b7f1cdb
Sittipatima <sittipatima958@gmail.com>
2024-10-11 20:38:25 +0800

    Create Day40.java

---
+++ Day40.java
@@ -0,0 +1,11 @@
+public class dayy40 {
+    public static void main(String[] args) {
+      int a = 8;
+      int b = 14;
+      //operator perbandingan lebih besar  
+      System.out.println(a > b);
+      //operator perbandingan lebih kecil 
+      System.out.println(a < b);
+    }
+   
+}
