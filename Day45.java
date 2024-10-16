cc857b09719d199b304857dd2441da4820475cdc
Sittipatima <sittipatima958@gmail.com>
2024-10-16 22:22:31 +0800

    Create Day45.java

---
+++ Day45.java
@@ -0,0 +1,8 @@
+public class day45{
+	public static void main(String[] args) {
+		boolean a = true || false;
+	    boolean b = false || false;
+	    System.out.println(a);
+	    System.out.println(b);
+	}
+}
