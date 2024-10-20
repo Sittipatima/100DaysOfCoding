12b320c0275f89507c066acb307ca5b76393f6d8
Sittipatima <sittipatima958@gmail.com>
2024-10-20 19:51:36 +0800

    Create Day49.java

---
+++ Day49.java
@@ -0,0 +1,10 @@
+public class Day49 {
+	public static void main(String[] args) {
+	int a = 3;
+	if (a>10) {
+		System.out.println("a lebih besar dari 10");
+	} else if (a<10) {
+		System.out.println("a lebih kecil dari 10");
+	}
+	
+	}
