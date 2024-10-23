3336193ffb90f6ce036579fbe86b9d19a77a255d
Sittipatima <sittipatima958@gmail.com>
2024-10-23 23:25:50 +0800

    Update Day52.java

--- Day52.java
+++ Day52.java
@@ -5,8 +5,7 @@ public class Day52 {
 	 Scanner input = new Scanner (System.in); 
    System.out.print("Masukkan Angka = ");
 	 int angka = input.nextInt();
-	 int hasil = angka % 2;
-	 String kondisi = hasil == 0?"Genap":"Ganjil";       
+	 String kondisi =(angka % 2 == 0)?"Genap":"Ganjil";       
    System.out.println(kondisi);
 	}
 }
