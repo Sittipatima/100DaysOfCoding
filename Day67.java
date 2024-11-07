87834fded3ffe15be6e46917f1de4fd4383b3ccf
Sittipatima <sittipatima958@gmail.com>
2024-11-07 22:35:37 +0800

    Create Day67.java

---
+++ Day67.java
@@ -0,0 +1,9 @@
+public class Day67 {
+    public static void cetakPesan() {
+        System.out.println("Halo");
+    }
+    public static void main(String[] args) {
+        cetakPesan();
+    }
+}
+
