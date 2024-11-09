0aff8b9807c4fbf7629f06c19696e1a7588b186c
Sittipatima <sittipatima958@gmail.com>
2024-11-09 13:39:07 +0800

    Update Day69.java

--- Day69.java
+++ Day69.java
@@ -70,3 +70,18 @@ public class Main {
 		System.out.println("hasil perkalian = "+ hasil);
 	}
 }
+
+//soal 6
+public class Main {
+    public static void main(String[] args){
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");  
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*"); 
+            }
+            System.out.println();
+        }
+    }
+}
