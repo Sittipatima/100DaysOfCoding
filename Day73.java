dd550be45abfd579acbe4b9511fdea5247c47162
Sittipatima <sittipatima958@gmail.com>
2024-11-13 21:32:30 +0800

    Create Day73.java

---
+++ Day73.java
@@ -0,0 +1,7 @@
+public class day73{
+    public static void main(String[] args) {
+        String aku = "fatima";
+        char huruf = aku.charAt(3);
+        System.out.println(huruf);   
+    }
+}
