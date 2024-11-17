39cc2491100f00e0dc584135ef799bd5baec1e58
Sittipatima <sittipatima958@gmail.com>
2024-11-17 20:51:35 +0800

    Create Day77.java

---
+++ Day77.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		String a = "Fatima";
+		String b = "Fatima";
+		String c = "FATIMA";
+		boolean nilai = a.equals(b);
+		boolean nilai2 = a.equals(c);
+		System.out.println(nilai);
+		System.out.println(nilai2);
