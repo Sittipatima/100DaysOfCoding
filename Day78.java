2ce60945a2d52e35068a8a0222ae7b0ced615c68
Sittipatima <sittipatima958@gmail.com>
2024-11-18 19:40:58 +0800

    Create Day78.java

---
+++ Day78.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		String a,b,c;
+		a ="FATIMA";
+		b = "fatima";
+		c = "Rusna";
+		System.out.println(a.equalsIgnoreCase(b));
+		System.out.println(a.equalsIgnoreCase(c));
+	}
+}
