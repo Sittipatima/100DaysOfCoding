7c85fef2611b1f26bc03787d4009f1c36fe3e16a
Sittipatima <sittipatima958@gmail.com>
2024-11-21 21:17:33 +0800

    Update Day81.java

--- Day81.java
+++ Day81.java
@@ -1,6 +1,7 @@
 public class day81 {
 	public static void main(String[] args) {
 		String a = "664";
-		System.out.println(Integer.valueOf(a));
+		int b = Integer.valueOf(a);
+		System.out.println(b);
 	}
 }
