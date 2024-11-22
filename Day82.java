939bd0c36879405cdd4f8bc16d4cbc49d24493cf
Sittipatima <sittipatima958@gmail.com>
2024-11-22 21:26:04 +0800

    Create Day82.java

--- Day82.java
+++ Day82.java
@@ -1,11 +1,10 @@
-import java.math.*;
 public class Main {
 	public static void main(String[] args) {
 		double a = 20.3;
 		double b = 15.8;
 		double c = 5.5;
-		System.out.println((Math.ceil(a)));
-		System.out.println((Math.floor(b)));
-		System.out.println((Math.round(c)));
+		System.out.println(Math.ceil(a));
+		System.out.println(Math.floor(b));
+		System.out.println(Math.round(c));
 	}
 }
