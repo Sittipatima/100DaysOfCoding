71bcce1480b055a77670468325a724de6789c21d
Sittipatima <sittipatima958@gmail.com>
2024-11-23 21:37:03 +0800

    Create Day83.java

--- Day83.java
+++ Day83.java
@@ -1,6 +1,6 @@
 public class day83{
 	public static void main(String[] args) {
-	 int [] angka = {2, 4, 6, 7, 8};
-	 System.out.println(angka[2]);	
+	 int [] angka = {2, 3, 4,};
+	 System.out.println(angka[1]);	
 	}
 }
