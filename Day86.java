2cd202ecad2129f74a3f59cc86dbcef497c02920
Sittipatima <sittipatima958@gmail.com>
2024-11-26 23:06:25 +0800

    Create Day86.java

---
+++ Day86.java
@@ -0,0 +1,8 @@
+public class day86 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        for (int a : array) {
+            System.out.print(a+" ");
+        }
+    }
+}
