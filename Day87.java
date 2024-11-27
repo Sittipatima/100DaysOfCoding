d32daa542eb75ac8f02d1ac86be492a639e87d8b
Sittipatima <sittipatima958@gmail.com>
2024-11-27 23:27:21 +0800

    Create Day87.java

---
+++ Day87.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        array[2] = 10;
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+
