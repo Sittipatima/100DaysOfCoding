e9fdb02067f20124be8e6ce8bc750c51d630a1cd
Sittipatima <sittipatima958@gmail.com>
2024-11-28 23:51:08 +0800

    Update Day88.java

--- Day88.java
+++ Day88.java
@@ -8,28 +8,18 @@ public class Soal1 {
         System.out.println("Masukkan gaji pokok : ");
         long gaji = input.nextLong();
         long diskon = 0;
-        if (gaji < 2000000 ){
-            status = "PNS";
-            diskon = 5*gaji/100;
-            gaji -= diskon;
-            System.out.println("anda mendapat diskon 5%");
-        }
-        if(gaji < 2000000){
-           status = "Bukan";
-            System.out.println("maaf anda tidak mendapat diskon");
-        }else if (gaji >= 2000000 && gaji <=5000000){
+         if (gaji >= 2000000 && gaji <=5000000){
             status = "PNS";
             diskon = 10*gaji/100;
             gaji -= diskon;
             System.out.println("anda mendapat diskon 10%");
-        }
-        if (gaji >= 2000000 && gaji <= 5000000){
+        }else if (gaji >= 2000000 && gaji <= 5000000){
             status = "Bukan";
             diskon = 5*gaji/100;
             gaji -= diskon;
             System.out.println("anda mendapat diskon 5%");
         }
-        if(gaji > 5000000){
+         if(gaji > 5000000){
             status = "PNS";
             diskon = 12*gaji/100;
             gaji -= diskon;
@@ -41,6 +31,15 @@ public class Soal1 {
             gaji -= diskon;
             System.out.println("anda mendapat diskon 7%");
         }
+        if (gaji < 2000000){
+            status = "PNS";
+            diskon = 5*gaji/100;
+            gaji -= diskon;
+            System.out.println("anda mendapat diskon 5%");
+        }else if (gaji < 2000000){
+           status = "Bukan";
+            System.out.println("maaf anda tidak mendapat diskon");
+        }
         System.out.println("Total gaji bersih Anda : "+gaji);
     }
-}
+ }
