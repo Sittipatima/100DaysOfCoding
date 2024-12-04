e5680799e02583f6f78e557c1154ff68dded4cc1
Sittipatima <sittipatima958@gmail.com>
2024-12-04 22:01:27 +0800

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class day94 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan tinggi segitiga: ");
+        int tinggi = in.nextInt();
+        for (int a = 1; a <= tinggi; a++) {
+            for (int b  = 1; b <= tinggi - a; b++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= (2 * i - 1); c++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
